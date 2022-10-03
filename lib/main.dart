51f11dae160038fbbd6ce0805e0b1d2b1845cdf3
Bjørn Wikkeling <bjorn@wikkeling.com>
2022-10-04 00:07:28 +0200

    updates app name

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Frontmania',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
